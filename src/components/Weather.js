47bac2376e7cb16a39dc524daf0365b53c226a08
rohit-virtueinfo <rohit.virtueinfo@gmail.com>
2024-02-19 19:59:36 +0530

    feat: Re-structure the code and fix some issue

---
+++ src/components/Weather.js
@@ -0,0 +1,73 @@
+import React from "react";
+import axios from "axios";
+
+const Weather = ({ location, setLocation, data, setData }) => {
+  const searchLocation = async (event) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
+    if (event.key === "Enter") {
+      await axios.get(url).then((response) => {
+        setData(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <>
+      <div className="search">
+        <input
+          className="serach-input"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="Enter Location"
+          type="text"
+        />
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+Weather.defaultProps = {
+  location: "",
+  setLocation: () => {},
+  data: null,
+  setData: () => {},
+};
+
+export default Weather;
